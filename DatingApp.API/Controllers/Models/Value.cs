a418a42b52ed1c3b749ad94ea7518f6e5374fe45
snchvz <004741853@coyote.csusb.edu>
2020-05-20 23:26:20 -0700

    init commit

---
+++ DatingApp.API/Controllers/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {   
+        //.net will infer that int id is the key by convention
+        public int id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ DatingAppSPA/src/app/value/value.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+  values: any; //property with type of any
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.GetValues();
+  }
+
+  GetValues()
+  {
+    //response is what we're getting back from our server
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+}
